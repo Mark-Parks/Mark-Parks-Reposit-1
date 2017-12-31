724407d51a8ba149463c3bf63706a4483e4c18e3
Mark Alessandro <Mark Alessandro@MarkAlessandro.fios-router.home>
2017-12-30 21:55:05 -0500

    MarkParks Starting Symposium Project

---
+++ MarkSymposium/src/SymposiumGame/SymposiumMain.java
@@ -0,0 +1,10 @@
+package SymposiumGame;
+
+public class SymposiumMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	//Hello This is the start of the process of making a game.
+}
